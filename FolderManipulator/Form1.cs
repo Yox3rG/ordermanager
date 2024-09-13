c834b96ef1a6648880d165233865da5fdd289992
Yox3rG <realyoxerg@gmail.com>
2024-09-13 12:06:20 +0200

    Removed unnecessary using.

--- FolderManipulator/Form1.cs
+++ FolderManipulator/Form1.cs
@@ -6,7 +6,6 @@ using FolderManipulator.UI;
 using System;
 using System.Collections.Generic;
 using System.Data;
-using System.Diagnostics;
 using System.Drawing;
 using System.IO;
 using System.Linq;
